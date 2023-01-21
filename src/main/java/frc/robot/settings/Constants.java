2c2baed4bc4ddce228ae0ee6c0c4ee1270e34446
frc2405-programmer <119539207+frc2405-programmer@users.noreply.github.com>
2023-01-21 12:15:43 -0500

    Add Constants class

---
+++ src/main/java/frc/robot/settings/Constants.java
@@ -0,0 +1,13 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.settings;
+
+/** Program-wide constants for the robot.
+ *  For settings that should be changed before
+ *  building and deploying the code.
+ */
+public class Constants {
+    
+}
