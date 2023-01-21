545025b35893dc24ba6ff96bd9cf79db1c9ee7a8
frc2405-programmer <119539207+frc2405-programmer@users.noreply.github.com>
2023-01-21 16:42:52 -0500

    more constants

--- src/main/java/frc/robot/settings/Constants.java
+++ src/main/java/frc/robot/settings/Constants.java
@@ -8,18 +8,48 @@ package frc.robot.settings;
  *  For settings that should be changed before
  *  building and deploying the code.
  */
+//TODO! Init values
 public final class Constants {
     /** This year we have to support multiple drivetrains, so each drivetrain is a subclass. */
     public static final class Drivetrains {
         public static final class Differential {
-            public static final class Motor {
-                public static final class Ports {
-                    //TODO! Init these values
-                    public static final int FRONT_LEFT = 0;
-                    public static final int FRONT_RIGHT = 0;
-                    public static final int BACK_LEFT = 0;
-                    public static final int BACK_RIGHT = 0;
+            public static final class Motors {               
+                public static final int FRONT_LEFT_PORT = 0;
+                public static final int FRONT_RIGHT_PORT = 0;
+                public static final int BACK_LEFT_PORT = 0;
+                public static final int BACK_RIGHT_PORT = 0;
+                
+                public static final class Encoders {
+                    public static final int FRONT_LEFT_PORT = 0;
+                    public static final int FRONT_RIGHT_PORT = 0;
+                    public static final int BACK_LEFT_PORT = 0;
+                    public static final int BACK_RIGHT_PORT = 0;
                 }
+            }
+        }
+
+        public static final class Swerve {
+            public static final class Motors {
+                public static final class Drive {
+                    public static final int FRONT_LEFT_PORT = 0;
+                    public static final int FRONT_RIGHT_PORT = 0;
+                    public static final int BACK_LEFT_PORT = 0;
+                    public static final int BACK_RIGHT_PORT = 0;
+                }
+
+                public static final class Turning {
+                    public static final int FRONT_LEFT_PORT = 0;
+                    public static final int FRONT_RIGHT_PORT = 0;
+                    public static final int BACK_LEFT_PORT = 0;
+                    public static final int BACK_RIGHT_PORT = 0;
+                }
+            }
+
+            public static final class EncoderShaft {
+                public static final int FRONT_LEFT_PORT = 0;
+                public static final int FRONT_RIGHT_PORT = 0;
+                public static final int BACK_LEFT_PORT = 0;
+                public static final int BACK_RIGHT_PORT = 0;
             }
         }
     }
