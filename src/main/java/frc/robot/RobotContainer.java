e9ab99a4443ca0633638e79566de705941903efb
frc2405-programmer <119539207+frc2405-programmer@users.noreply.github.com>
2023-01-24 18:47:16 -0500

    Add Controls for Differential Drive

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,17 +4,36 @@
 
 package frc.robot;
 
+import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.RunCommand;
+import frc.robot.subsystems.drivetrains.Differential;
 
 public class RobotContainer {
+
+  private Differential differentialDrive;
+  private XboxController driverController;
+
+
   public RobotContainer() {
     configureBindings();
+    differentialDrive = new Differential();
+    differentialDrive.setDefaultCommand(getDifferentialCommand());
   }
 
-  private void configureBindings() {}
+  private void configureBindings() {
+    driverController = new XboxController(0); //TODO! convert to constant
+  }
 
   public Command getAutonomousCommand() {
     return Commands.print("No autonomous command configured");
+  }
+
+  public Command getDifferentialCommand() {
+    return new RunCommand(() -> differentialDrive.DriveTank(
+      driverController.getLeftY(),
+      driverController.getRightY()
+    ));
   }
 }
