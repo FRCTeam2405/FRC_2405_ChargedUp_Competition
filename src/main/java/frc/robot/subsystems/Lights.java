8684a3f519bf8278f81d38e19bee8f6f4330bcd2
frc2405-programmer <119539207+frc2405-programmer@users.noreply.github.com>
2023-01-23 19:41:48 -0500

    Add basic functionality

--- src/main/java/frc/robot/subsystems/Lights.java
+++ src/main/java/frc/robot/subsystems/Lights.java
@@ -3,10 +3,14 @@
 // the WPILib BSD license file in the root directory of this project.
 
 package frc.robot.subsystems;
-
+import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.settings.Constants;
 
 public class Lights extends SubsystemBase {
+
+  private PWMSparkMax rawLights = new PWMSparkMax(Constants.LEDs.PORT);
+
   /** Creates a new Lights subsystem for managing the robot LEDs. */
   public Lights() {}
 
@@ -14,4 +18,13 @@ public class Lights extends SubsystemBase {
   public void periodic() {
     // This method will be called once per scheduler run
   }
+
+  public void setColor(double color) {
+    rawLights.set(color);
+  }
+
+  public double getColor() {
+    return rawLights.get();
+  }
+
 }
