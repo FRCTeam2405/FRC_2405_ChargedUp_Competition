305e6d4df231f1e8420de1324ce665d2b4803407
frc2405-programmer <119539207+frc2405-programmer@users.noreply.github.com>
2023-01-23 20:15:27 -0500

    WIP

--- src/main/java/frc/robot/settings/Constants.java
+++ src/main/java/frc/robot/settings/Constants.java
@@ -4,6 +4,10 @@
 
 package frc.robot.settings;
 
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
+import edu.wpi.first.math.util.Units;
+
 /** Program-wide constants for the robot.
  *  For settings that should be changed before
  *  building and deploying the code.
@@ -29,8 +33,29 @@ public final class Constants {
         }
 
         public static final class Swerve {
+            public static final class Odometry {
+            //TODO! FIX ALL
+                // Chassis configuration
+                public static final double kTrackWidth = Units.inchesToMeters(26.5);
+                // Distance between centers of right and left wheels on robot
+                public static final double kWheelBase = Units.inchesToMeters(26.5);
+                // Distance between front and back wheels on robot
+                public static final SwerveDriveKinematics kDriveKinematics = new SwerveDriveKinematics(
+                new Translation2d(kWheelBase / 2, kTrackWidth / 2),
+                new Translation2d(kWheelBase / 2, -kTrackWidth / 2),
+                new Translation2d(-kWheelBase / 2, kTrackWidth / 2),
+                new Translation2d(-kWheelBase / 2, -kTrackWidth / 2));
+
+                // Angular offsets of the modules relative to the chassis in radians
+                public static final double kFrontLeftChassisAngularOffset = -Math.PI / 2;
+                public static final double kFrontRightChassisAngularOffset = 0;
+                public static final double kBackLeftChassisAngularOffset = Math.PI;
+                public static final double kBackRightChassisAngularOffset = Math.PI / 2;
+            }
+
             public static final class Motors {
                 public static final class Drive {
+                    //TODO! fix later
                     public static final int FRONT_LEFT_PORT = 0;
                     public static final int FRONT_RIGHT_PORT = 0;
                     public static final int BACK_LEFT_PORT = 0;
@@ -38,6 +63,7 @@ public final class Constants {
                 }
 
                 public static final class Turning {
+                    //TODO! fix later
                     public static final int FRONT_LEFT_PORT = 0;
                     public static final int FRONT_RIGHT_PORT = 0;
                     public static final int BACK_LEFT_PORT = 0;
@@ -45,7 +71,9 @@ public final class Constants {
                 }
             }
 
-            public static final class EncoderShaft {
+            public static final class Encoders {
+                //TODO! fix later
+                public final int QUAD_COUNTS_PER_ROTATION = 4096;
                 public static final int FRONT_LEFT_PORT = 0;
                 public static final int FRONT_RIGHT_PORT = 0;
                 public static final int BACK_LEFT_PORT = 0;
