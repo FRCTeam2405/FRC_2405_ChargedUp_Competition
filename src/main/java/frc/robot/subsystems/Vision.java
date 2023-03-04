2485b411e08c2e12a075cef4b74c53316bd04e1a
frc2405-programmer <2405.sk07@proton.me>
2023-03-04 11:19:05 -0500

    Testing jetson

--- src/main/java/frc/robot/subsystems/Vision.java
+++ src/main/java/frc/robot/subsystems/Vision.java
@@ -4,14 +4,35 @@
 
 package frc.robot.subsystems;
 
+import edu.wpi.first.networktables.DoubleArraySubscriber;
+import edu.wpi.first.networktables.FloatArraySubscriber;
+import edu.wpi.first.networktables.IntegerArraySubscriber;
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Vision extends SubsystemBase {
+
+  NetworkTable jetson;
+
+  DoubleArraySubscriber distances;
+  DoubleArraySubscriber bearings;
+  IntegerArraySubscriber categories;
+
   /** Creates a new Vision. */
-  public Vision() {}
+  public Vision() {
+    jetson = NetworkTableInstance.getDefault().getTable("Jetson");
+
+    distances = jetson.getDoubleArrayTopic("Distance").subscribe(new double[] {});
+    bearings = jetson.getDoubleArrayTopic("Bearing").subscribe(new double[] {});
+    categories = jetson.getIntegerArrayTopic("Category").subscribe(new long[] {});
+  }
 
   @Override
   public void periodic() {
-    // This method will be called once per scheduler run
+    SmartDashboard.putNumber("distance0", distances.get()[0]);
+    SmartDashboard.putNumber("bearing0", bearings.get()[0]);
+    SmartDashboard.putNumber("category0", categories.get()[0]);
   }
 }
