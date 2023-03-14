9c013dc45ac6d28e39a0fbb30e2fdfd6ddec4b10
frc2405-programmer <119539207+frc2405-programmer@users.noreply.github.com>
2023-03-14 19:38:43 -0400

    fix?

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -10,6 +10,7 @@ import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import frc.robot.commands.swerve.AbsoluteDrive;
 import frc.robot.commands.swerve.SwerveAutobalence;
@@ -31,11 +32,11 @@ public class RobotContainer {
   private Joystick driverLeftStick = new Joystick(Constants.Controllers.DRIVER_CONTROLLER_PORT);
   private Joystick driverRightStick = new Joystick(Constants.Controllers.DRIVER_CONTROLLER_PORT);
 
-  private XboxController driverController = new XboxController(Constants.Controllers.DRIVER_CONTROLLER_PORT);
-  private final JoystickButton driverMainButtonX = new JoystickButton(driverController, Constants.Controllers.DriverController.DRIVER_CONTROLLER_BUTTON_X);
-  private final JoystickButton driverMainButtonB = new JoystickButton(driverController, Constants.Controllers.DriverController.DRIVER_CONTROLLER_BUTTON_B);
-  private final JoystickButton driverMainButtonY = new JoystickButton(driverController, Constants.Controllers.DriverController.DRIVER_CONTROLLER_BUTTON_Y);
-  private final JoystickButton driverMainButtonA = new JoystickButton(driverController, Constants.Controllers.DriverController.DRIVER_CONTROLLER_BUTTON_A);
+  private CommandXboxController driverController = new CommandXboxController(Constants.Controllers.DRIVER_CONTROLLER_PORT);
+  // private final JoystickButton driverMainButtonX = new JoystickButton(driverController, Constants.Controllers.DriverController.DRIVER_CONTROLLER_BUTTON_X);
+  // private final JoystickButton driverMainButtonB = new JoystickButton(driverController, Constants.Controllers.DriverController.DRIVER_CONTROLLER_BUTTON_B);
+  // private final JoystickButton driverMainButtonY = new JoystickButton(driverController, Constants.Controllers.DriverController.DRIVER_CONTROLLER_BUTTON_Y);
+  // private final JoystickButton driverMainButtonA = new JoystickButton(driverController, Constants.Controllers.DriverController.DRIVER_CONTROLLER_BUTTON_A);
 
   public RobotContainer() {
 
@@ -48,13 +49,13 @@ public class RobotContainer {
     configureBindings();
 
     // Set default commands
-    swerveDrive.setDefaultCommand(new AbsoluteDrive(
-      swerveDrive,
-      axisDeadband(driverController, XboxController.Axis.kLeftY.value),
-      axisDeadband(driverController, XboxController.Axis.kLeftX.value),
-      axisRaw(driverController, XboxController.Axis.kRightX.value),
-      axisRaw(driverController, XboxController.Axis.kRightY.value)
-    ));
+    // swerveDrive.setDefaultCommand(new AbsoluteDrive(
+    //   swerveDrive,
+    //   axisDeadband(driverController, XboxController.Axis.kLeftY.value),
+    //   axisDeadband(driverController, XboxController.Axis.kLeftX.value),
+    //   axisRaw(driverController, XboxController.Axis.kRightX.value),
+    //   axisRaw(driverController, XboxController.Axis.kRightY.value)
+    // ));
   }
 
   private DoubleSupplier axisDeadband(XboxController controller, int axis) {
@@ -71,7 +72,7 @@ public class RobotContainer {
     //driverMainButtonX.whenHeld();
     //driverMainButtonB.whenHeld();
     //driverMainButtonY.whenHeld();
-    driverMainButtonA.whenPressed(commandBalence);
+    driverController.a().onTrue(commandBalence);
 
 
   }
