a7efd2797151377a839504248e8629a1d9ade4a1
frc2405-programmer <2405.sk07@proton.me>
2023-03-21 18:41:59 -0400

    arbitrary change is arbitrary

--- src/main/java/frc/robot/Commands/autonomous/ForwardDock.java
+++ src/main/java/frc/robot/Commands/autonomous/ForwardDock.java
@@ -30,7 +30,7 @@ public class ForwardDock extends CommandBase {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    if(Math.abs(swerve.getPitch().getDegrees()) > 11) {
+    if(Math.abs(swerve.getPitch().getDegrees()) > 14) {
       onChargeStation = true;
       startPosition = swerve.getPose().getTranslation();
     }
