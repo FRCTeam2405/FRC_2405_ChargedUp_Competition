2753d0c7efe51c2a3f89824368aefbc5bc36b997
frc2405-programmer <2405.sk07@proton.me>
2023-03-21 17:51:25 -0400

    reorganize

---
+++ src/main/java/frc/robot/Commands/autonomous/arm/AutoOutputPiece.java
@@ -0,0 +1,25 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands.autonomous.arm;
+
+import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.ParallelRaceGroup;
+import frc.robot.commands.intake.grip.OutputPiece;
+import frc.robot.subsystems.intake.Grip;
+
+// NOTE:  Consider using this command inline, rather than writing a subclass.  For more
+// information, see:
+// https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
+public class AutoOutputPiece extends ParallelRaceGroup {
+  /** Creates a new AutoOutputPiece. */
+  public AutoOutputPiece(Grip intake) {
+    // Add your commands in the addCommands() call, e.g.
+    // addCommands(new FooCommand(), new BarCommand());
+    addCommands(
+      new OutputPiece(intake),
+      Commands.waitSeconds(2.0) // Time for intake to run for
+    );
+  }
+}
