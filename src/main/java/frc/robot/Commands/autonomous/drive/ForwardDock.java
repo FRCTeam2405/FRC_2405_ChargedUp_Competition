e9e8e857938cdfbfc4ccd4010a7071b93e6546eb
frc2405-programmer <2405.sk07@proton.me>
2023-03-27 19:52:06 -0400

    wasdfghjkhgdfsafghjkl

--- src/main/java/frc/robot/Commands/autonomous/drive/ForwardDock.java
+++ src/main/java/frc/robot/Commands/autonomous/drive/ForwardDock.java
@@ -5,6 +5,7 @@
 package frc.robot.commands.autonomous.drive;
 
 import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.settings.Constants.LEDs.Colors;
@@ -19,6 +20,8 @@ public class ForwardDock extends CommandBase {
   private boolean onChargeStation = false;
   private boolean finished = false;
   private Translation2d startPosition;
+
+  private Timer timer = new Timer();
 
   /** Creates a new ForwardDock. */
   public ForwardDock(SwerveContainer swerve, Lights lights) {
@@ -37,6 +40,12 @@ public class ForwardDock extends CommandBase {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
+
+    if(finished) {
+      driveSideways();
+      return;
+    }
+
     if(Math.abs(swerve.getPitch().getDegrees() - 360) > 7) {
       onChargeStation = true;
       startPosition = swerve.getPose().getTranslation();
@@ -44,9 +53,11 @@ public class ForwardDock extends CommandBase {
     }
 
     if(!onChargeStation) {
-      swerve.drive(0.25, 0, 0);
+      swerve.drive(0.15, 0, 0);
       return;
     }
+
+
 
     double distance =
       swerve.getPose().getTranslation().getX() -
@@ -54,27 +65,43 @@ public class ForwardDock extends CommandBase {
 
     SmartDashboard.putNumber("autoDistance", distance);
 
-    if(Math.abs(distance) > 0.02) {
-      swerve.drive(-0.05, 0, 0);
+    if(Math.abs(distance) > 0.01) {
+      swerve.drive(0, 0.001, 0);
       swerve.setBrakes(true);
       lights.setColor(Colors.GREEN);
 
       finished = true;
+      timer.start();
       return;
     }
 
-    swerve.drive(0.1, 0, 0);
+
+
+    swerve.drive(0.05, 0, 0);
   }
 
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
-    
+    finished = false;
+    timer.stop();
   }
 
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    return finished;
+    return false;
+  }
+
+  private void driveSideways() {
+    if(timer.get() > 0.1) {
+      swerve.drive(-0.01, 0, 0);
+      swerve.setBrakes(true);
+      lights.setColor(Colors.GREEN);
+    } else {
+      swerve.drive(0, 0, 0);
+      swerve.setBrakes(true);
+    }
+    
   }
 }
